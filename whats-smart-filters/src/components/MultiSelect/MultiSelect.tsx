d812a50fde087362c7eaa7a3c5497c3f8f9c6003
rodolfo9254 <rodolfo9254@gmail.com>
2025-01-01 20:27:28 -0300

    multiselect css, label and options

---
+++ whats-smart-filters/src/components/MultiSelect/MultiSelect.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import "./MultiSelect.css"
+
+interface Option {
+  label: string;
+  value: string;
+}
+
+interface MultiSelectProps {
+  options: Option[];
+  label: string;
+}
+
+const MultiSelect: React.FC<MultiSelectProps> = ({ options, label }) => {
+  const [selectedItems, setSelectedItems] = useState<string[]>([]);
+
+  const handleSelect = (item: string) => {
+    setSelectedItems((prevSelectedItems) =>
+      prevSelectedItems.includes(item)
+        ? prevSelectedItems.filter((i) => i !== item)
+        : [...prevSelectedItems, item]
+    );
+  };
+
+  return (
+    <div className="dropdown">
+      <label className="txt-grey">{label}</label>
+
+      <button
+        className="btn btn-custom dropdown-toggle w-100 txt-white"
+        type="button"
+        id="dropdownMenuButton"
+        data-bs-toggle="dropdown"
+        aria-haspopup="true"
+        aria-expanded="false"
+        disabled={!options}
+      >
+        Usuários
+      </button>
+      <ul className="dropdown-menu dropdown-menu-custom w-100" aria-labelledby="dropdownMenuButton">
+        {options.map((option) => (
+          <li key={option.value}>
+            <a
+              className={`dropdown-item dropdown-item-custom d-flex align-items-center`}
+              href="#"
+              onClick={(e) => {
+                e.preventDefault(); // Para não recarregar a página
+                handleSelect(option.value);
+              }}
+            >
+              <input
+                type="checkbox"
+                checked={selectedItems.includes(option.value)}
+                onChange={() => handleSelect(option.value)}
+                className="me-2"
+              />
+              {option.label}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MultiSelect;
--- whats-smart-filters/src/main.tsx
+++ whats-smart-filters/src/main.tsx
@@ -4,6 +4,8 @@ import { createRoot } from 'react-dom/client'
 // bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
+
 
 import './index.css'
 import App from './App.tsx'
