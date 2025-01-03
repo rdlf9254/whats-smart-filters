d374c9ea4f8a8442e4dc4be093a556a720d69db9
rodolfo9254 <rodolfo9254@gmail.com>
2025-01-03 11:27:34 -0300

    filter interface

---
+++ whats-smart-filters/src/types/filter.ts
@@ -0,0 +1,10 @@
+export default interface Filter {
+    users: string[];
+    dateStart: string;
+    dateEnd: string;
+    timeStart:string;
+    timeEnd:string;
+    contain: string;
+    notContain: string;
+  }
+  
