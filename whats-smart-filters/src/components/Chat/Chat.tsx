3aca714e1bae8f1b7924268ee2a5cb67fb674efc
rodolfo9254 <rodolfo9254@gmail.com>
2025-01-02 21:14:32 -0300

    fix has messages

--- whats-smart-filters/src/components/Chat/Chat.tsx
+++ whats-smart-filters/src/components/Chat/Chat.tsx
@@ -6,31 +6,33 @@ import { ChatContext } from "../../contexts/ChatContext";
 const Chat: React.FC = () => {
   const context = useContext(ChatContext);
 
-  return (
-    <div className="chat  px-4 py-1">
-      {/* with file uploaded: */}
-      <div className="chat-container pt-4">
-        {context?.messages.map((message, i) => (
-          <MessageBox
-            key={i}
-            message={message.message}
-            user={message.user}
-            date={message.date}
-            time={message.time}
-            color={message.color || "#888"}
-          ></MessageBox>
-        ))}
-      </div>
+  const hasMessages = context?.messages && context.messages.length > 0;
 
-      {/* no file uploaded message: */}
-      <div className="d-flex txt-grey flex-column justify-content-center align-items-center h-100">
-        <i className="bi bi-inbox empty-icon"></i>
-        <h2>Nenhum arquivo encontrado</h2>
-        <p className="text-center">
-          Você ainda não fez o upload de nenhum arquivo .txt. Clique no botão ao
-          lado para começar!
-        </p>
-      </div>
+  return (
+    <div className="chat px-4 py-1">
+      {hasMessages ? (
+        <div className="chat-container pt-4">
+          {context.messages.map((message, i) => (
+            <MessageBox
+              key={i}
+              message={message.message}
+              user={message.user}
+              date={message.date}
+              time={message.time}
+              color={message.color || "#888"}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="d-flex txt-grey flex-column justify-content-center align-items-center h-100">
+          <i className="bi bi-inbox empty-icon"></i>
+          <h2>Nenhum arquivo encontrado</h2>
+          <p className="text-center">
+            Você ainda não fez o upload de nenhum arquivo .txt. Clique no botão
+            ao lado para começar!
+          </p>
+        </div>
+      )}
     </div>
   );
 };
