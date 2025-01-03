102df93ca1c3b6ebc3e5f4b9ec6ce333b978f37d
rodolfo9254 <rodolfo9254@gmail.com>
2025-01-02 21:04:22 -0300

    fix message box

--- whats-smart-filters/src/components/MessageBox/MessageBox.tsx
+++ whats-smart-filters/src/components/MessageBox/MessageBox.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { formatDate } from "../../utils/dateUtils";
 import Message from "../../types/Message";
+import './MessageBox.css'
 
 const MessageBox: React.FC<Message> = ({
   user,
@@ -14,11 +15,10 @@ const MessageBox: React.FC<Message> = ({
       <div className="msg-header  fs-6">
         <span className="txt-white txt-sm">{user}</span>
         <span className="txt-grey ms-auto pl-1 txt-sm">{formatDate(date,time)}</span>
-        {time}
       </div>
 
       <div
-        className="rounded p-2 mt-2 fs-6 text-dark text-wrap"
+        className="msg-box rounded p-2 mt-2 fs-6 text-dark text-wrap"
         style={{ backgroundColor: color }}
       >
         {message}
